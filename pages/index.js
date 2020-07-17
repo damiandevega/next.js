207e022a0b81c28ba33cd51d9eaf83e3d3a6dbb9
Damian de Vega <damiandevega@gmail.com>
2020-07-17 10:40:39 -0400

    getting initial props context

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,20 @@ import Link from 'next/link';
 import Router from 'next/router';
 
 class IndexPage extends Component {
+  static getInitialProps(context) {
+    console.log(context);
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({ appName: 'Super App' });
+      }, 10);
+    });
+    return promise;
+  }
+
   render() {
     return (
       <div>
-        <h1>The Main Page</h1>
+        <h1>The Main Page of {this.props.appName}</h1>
         <p>
           Go to{' '}
           <Link href="/auth">
